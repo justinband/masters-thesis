28f6440034796c1257a51f52e0d63a70d8bd15f9
Justin Band <jstinband@gmail.com>
2025-02-19 14:55:57 +0100

    update step to handle deadlines

--- mdps/job_mdp.py
+++ mdps/job_mdp.py
@@ -1,11 +1,14 @@
+from pandas import DataFrame
+from datasets import DataLoader
 import numpy as np
 
 class JobMDP():
-    def __init__(self, job_size):
+    def __init__(self, job_size, energy_df):
         # MDP Parameters
         self.nS = job_size  # num states
         self.nA = 2         # num actions
         self.s = 0          # initial state
+        self.time = 0       # current run time
 
         # Actions
         self.pause = 0
@@ -13,6 +16,10 @@ class JobMDP():
 
         # Flags
         self.complete = False
+
+        # Energy Data
+        self.energy = energy_df
+        self.curr_loss = None
 
         # Transiton Matrix
         self.p = np.zeros((self.nS, self.nA, self.nS))
@@ -30,15 +37,36 @@ class JobMDP():
         return np.argmax(self.p[self.s, self.run]) # Assumes that states are known
 
     def step(self, a):
-        if ((a == self.run) and (self.s == self.nS - 1)) or self.complete:
+        """
+        Execute run/pause action in the MDP.
+
+        Running in the last state advances the MDP to a complete/terminal state.
+        """
+        ### If deadline reached
+        if self.time >= len(self.energy):
+            print("Deadline has been reached.")
             self.complete = True
-            print(f"Complete - s={self.s}")
-            return self.s, self.complete
-        else:
-            self.s = np.argmax(self.p[self.s, a]) # Deterministic selection.
-            return self.s, self.complete
+            return -1, -1, self.complete 
         
-    def reset(self):
+        ### Otherwise, execute action
+        self.curr_loss = self.energy.iloc[self.time]
+
+        if ((a == self.run) and (self.s == self.nS - 1)) or self.complete: # Goes to terminal state or completed
+            self.complete = True
+            print("Job is complete:")
+        else: # Perform action
+            self.s = np.argmax(self.p[self.s, a]) # deterministic selection
+
+        self.time += 1
+
+        return self.s, self.curr_loss, self.complete
+        
+    def reset(self, energy_df=None):
         self.s = 0
+        self.time = 0
+        self.curr_loss = self.energy.iloc[self.time]
         self.complete = False
+        if energy_df:
+            self.energy = energy_df
+
         return self.s
