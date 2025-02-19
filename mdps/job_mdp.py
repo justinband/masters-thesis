40ea4702d01cd05d2fd4a0b39e6dbc1871c1cfe6
Justin Band <jstinband@gmail.com>
2025-02-19 17:11:47 +0100

    improve deadline handling and add loss function to mdp

--- mdps/job_mdp.py
+++ mdps/job_mdp.py
@@ -33,6 +33,13 @@ class JobMDP():
 
     def get_next_state(self):
         return np.argmax(self.p[self.s, self.run]) # Assumes that states are known
+    
+    def get_loss(self, a=None):
+        if (a is not None) and (a == self.pause):
+            return 0
+        else:
+            return self.energy['normalized'].iloc[self.time]
+
 
     def step(self, a):
         """
@@ -40,21 +47,21 @@ class JobMDP():
 
         Running in the last state advances the MDP to a complete/terminal state.
         """
-        ### If deadline reached
-        if self.time >= len(self.energy):
-            print("Deadline has been reached.")
+        self.curr_loss = self.get_loss(a)
+
+        # Terminating Action - running in last state reaches terminal state
+        if ((a == self.run) and (self.s == self.nS - 1)) or self.complete: # Terminal state
             self.complete = True
-            return -1, -1, self.complete 
+            return self.s, self.curr_loss, self.complete
         
-        ### Otherwise, execute action
-        self.curr_loss = self.energy['normalized'].iloc[self.time]
-
-        if ((a == self.run) and (self.s == self.nS - 1)) or self.complete: # Goes to terminal state or completed
-            self.complete = True
-        else: # Perform action
-            self.s = np.argmax(self.p[self.s, a]) # deterministic selection
-
+        # Non-terminating action
+        self.s = np.argmax(self.p[self.s, a]) # deterministic selection
+        self.curr_loss = self.get_loss(a)
         self.time += 1
+
+        # Deadline reached - last possible action
+        if self.time == len(self.energy): 
+            self.complete = True
 
         return self.s, self.curr_loss, self.complete
         
@@ -64,6 +71,6 @@ class JobMDP():
         self.complete = False
         if energy_df is not None:
             self.energy = energy_df
-            self.curr_loss = self.energy['normalized'].iloc[self.time]
+            self.curr_loss = self.get_loss()
 
         return self.s
