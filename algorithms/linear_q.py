9f76ab76044aea19fc681f7a5d4e02e82e838af5
Justin Band <jstinband@gmail.com>
2025-03-05 16:52:32 +0100

    use latency and trends in linq

--- algorithms/linear_q.py
+++ algorithms/linear_q.py
@@ -1,29 +1,38 @@
 import numpy as np
 from algorithms import LearningAlg
+from scipy.stats import linregress
+from collections import deque
 
 class LinearQ(LearningAlg):
 
-    def __init__(self, env, state_dim, action_dim = 2, alpha = 1e-6, epsilon = 0.1, latency=0):
+    def __init__(self, env, alpha = 1e-6, epsilon = 0.1, latency=0):
         super().__init__(env, epsilon, alpha, latency)
 
-        self.state_dim = state_dim
-        self.action_dim = action_dim
+        self.state_dim = 4
+        self.action_dim = 2
 
         self.w = np.zeros((self.action_dim, self.state_dim))  # Linear weights
         # self.w = np.zeros((self.action_dim, self.state_dim))  # Linear weights
 
-    def create_state(self, progress, curr_intensity):
+    def create_state(self, progress, intensity_q, latency):
         '''
         This function incorporates a variety of things into the state.
 
         For example, job progress and the current trend may be included.
         '''
-        return np.array([progress, curr_intensity])
+        slope = np.nan_to_num(linregress(x=np.arange(len(intensity_q)), y=intensity_q).slope)
+        curr_intensity = intensity_q[-1]
+
+        out_state = np.array([progress, curr_intensity, slope, latency])
+        assert len(out_state) == self.state_dim, AssertionError(f"State creation must be size {self.state_dim}. Currently is size {len(out_state)}.")
+
+        return out_state
     
     def choose_action(self, state, curr_latency):
-        if (self.max_latency == 0) or (curr_latency >= self.max_latency):
-            return self.env.run
-        elif np.random.rand() < self.epsilon:
+        # if (self.max_latency == 0) or (curr_latency >= self.max_latency):
+        #     return self.env.run
+        
+        if np.random.rand() < self.epsilon:
             return np.random.choice(self.action_dim)
         else:
             q_vals = np.dot(self.w, state)
@@ -40,21 +49,30 @@ class LinearQ(LearningAlg):
         progress = self.env.reset(start_idx)
         is_done = False
         episode_losses = []
+        
+        intensity_q = deque(maxlen=2)
 
         while not is_done:
+            # Get State
             curr_intensity = self.env.get_loss()
             curr_latency = self.env.get_latency()
+            intensity_q.append(curr_intensity) # Add curr intensity to Queue
 
-            state = self.create_state(progress, curr_intensity)
+            state = self.create_state(progress, intensity_q, curr_latency)
+            # state = self.create_state(progress, curr_intensity, prev_intensity)
 
+            # Select Action
             action = self.choose_action(state, curr_latency)
 
-            next_state, loss, is_done = self.env.step(action)
+            # Execute Action
+            s_prime, loss, is_done = self.env.step(action)
 
-            self.update_weights(state, action, loss, next_state)
+            # Update the weights
+            self.update_weights(state, action, loss, s_prime)
 
+            # Update tracking
             episode_losses.append(loss)
-            state = next_state
+            progress = s_prime
 
         return episode_losses, self.env.time
     
